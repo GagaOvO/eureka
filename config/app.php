097764ddca5812b0a3acf0e1589fd7c88473873b
Your Name <you@example.com>
2017-08-07 17:07:41 +0200

    okay

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Realtime Chat with Laravel, Vue and Pusher',
+    'name' => 'Eureka Business Chat Room',
 
     /*
     |--------------------------------------------------------------------------
