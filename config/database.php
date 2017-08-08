b7781a2cfd53c32f88aba061328450d22f583afb
Your Name <you@example.com>
2017-08-08 10:04:10 +0200

    Changed Ip address

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '138.197.165.79'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'qazmpvuyjt'),
             'username' => env('DB_USERNAME', 'qazmpvuyjt'),
